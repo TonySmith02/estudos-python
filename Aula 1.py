f7285491dbd7367ec1b784e2e042a1934ffb7447
Tony-Trabalho <tony.smith.31122002@gmail.com>
2026-01-27 17:58:44 -0300

    Finalização da Aula 1

--- Aula 1.py
+++ Aula 1.py
@@ -4,3 +4,21 @@ No mundo da programação, é uma tradição começar realizando um programa sim
 
 print("Olá Mundo!")
 
+# Identação
+## No Python, a identação(Espaços ou tabulações no início de uma linha) é utilizada para delimitar blocos de códigos. Diferente de outras linguagens que utilizam chaves ou paravras-chave, o Python utiliza a identação para determinar o escopo das declareções. Por exemplo:
+
+if condition:
+    # Bloco de código se a condição for verdadeira
+
+    '''
+    Instrução 1
+    Instrução 2
+    '''
+
+else
+    # Bloco de código se a condição for falsa
+
+    '''
+    Instrução 3
+    Instrução 4
+    '''
