887883d2b6a387be8c889d71f207af6e34e7fb7f
Tony-Trabalho <tony.smith.31122002@gmail.com>
2026-01-28 20:02:52 -0300

    Inicio da Aula 2

---
+++ Aula 2.py
@@ -0,0 +1,13 @@
+# Noções básicas de Python
+
+'''
+#TIPOS DE DADOS BÁSICOS
+Em Python, os tipos de dados básicos são as categorias nas quais podemos classificar os valores que utilizamos em nossos programas.
+Compreender os diferentes tipos de dados é fundamental para trabalhar com variáveis e realizar operações em Python.
+
+Os tipos de dados básicos incluem:
+
+
+## Inteiros (int)
+Os números inteiros são aqueles que não tem parte
+'''
